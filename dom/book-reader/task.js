af3c36a84e343452d0dc2af9fc8c03d3e09d6d9c
Daniil <sarychev-daniil@yandex.ru>
2024-03-06 18:24:29 +0300

    third task redone

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,3 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const fontSizeButtons = document.querySelectorAll('.font-size');
+
+  fontSizeButtons.forEach(function(button) {
+    button.addEventListener('click', function(event) {
+      event.preventDefault();
+      const size = this.getAttribute('data-size');
+
+      document.querySelector('.book').classList.remove('book_fs-big', 'book_fs-small');
+      document.querySelector('.book').classList.add('book_fs-' + size);
+
+      fontSizeButtons.forEach(function(button) {
+        button.classList.remove('font-size_active');
+      });
+
+      this.classList.add('font-size_active');
+    });
+  });
+}); 
+
+
 /*
 document.addEventListener('DOMContentLoaded', function() {
     const fontSizeButtons = document.querySelectorAll('.font-size');
@@ -16,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   */
 
-  document.addEventListener('DOMContentLoaded', function() {
+  /*document.addEventListener('DOMContentLoaded', function() {
     const fontSizeButtons = document.querySelectorAll('.font-size');
   
     fontSizeButtons.forEach(function(button) {
@@ -34,3 +55,23 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+  */
+
+
+  /*const btn = Array.from(document.querySelectorAll('.font-size'));
+  let size;
+  
+  btn.forEach(value => {
+    value.addEventListener('click', selectBtn)
+  })
+  
+  function selectBtn(event) {
+    btn.forEach(value => {
+      value.classList.remove('font-size_active')
+    })
+  
+    this.classList.add('font-size_active');
+    size = this.getAttribute('data-size');
+    event.preventDefault()
+  }
+  */
